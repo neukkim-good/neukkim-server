48329ff2608c086bedb5a31c04a50feb4bde2b7a
kmkkkp <kmkkkp@ajou.ac.kr>
2025-06-25 16:36:52 +0900

    ADD GET /ranking

---
+++ routes/ranking.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const Record = require("../models/Record");
+const router = express.Router();
+
+router.get("/", async function (req, res, next) {
+  console.log(Record);
+  const record = await Record.find();
+
+  res.json(record);
+});
+
+module.exports = router;
