30386c77316fe9b27dcdbe246db58028321cda0f
hanuuuuU <han980004@naver.com>
2025-06-27 15:35:19 +0900

    notify in ranking.js complete

--- models/Room.js
+++ models/Room.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const roomSchema = new mongoose.Schema({
   title: { type: String, required: true },
   maxUser: { type: Number, required: true },
-  host_id: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+  host_id: { type: mongoose.Types.ObjectId, ref: "User" },
   endTime: { type: Date, required: true },
   board: { type: [Number], required: true }, // board는 숫자 배열
 });
