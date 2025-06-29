13d675f7340de4b3dd29679214e3a41c69a8ffa3
klklgooa <klklgooa@gmail.com>
2025-06-29 22:10:49 +0900

    design

--- models/GameResult.js
+++ models/GameResult.js
@@ -4,7 +4,9 @@ const gameResultSchema = new mongoose.Schema({
   room_id: { type: mongoose.Types.ObjectId, required: true, ref: "Room" },
   user_id: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
   score: { type: Number, default: 0 },
+  endTime: { type: Date, required: true },
 });
+
 const GameResult = mongoose.model("GameResult", gameResultSchema, "GameResult");
 module.exports = GameResult;
 
